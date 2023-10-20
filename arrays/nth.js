e12c30c79180d9a850d6852105811277b1b72f09
christina <christinamharris83@gmail.com>
2023-10-20 11:23:48 -0700

    arrays

---
+++ arrays/nth.js
@@ -0,0 +1,17 @@
+// JS
+// challenger
+
+// Get nth element of array
+// Write a function that takes an array (a) and a value (n) as argument. Return the nth element of 'a'
+
+function myFunction(a, n) {
+    return a[n-1]
+    }
+    
+    // Test Cases:
+    console.log(myFunction([1,2,3,4,5],3));
+    console.log(myFunction([10,9,8,7,6],5));
+    console.log(myFunction([7,2,1,6,3],1));
+    
+
+
